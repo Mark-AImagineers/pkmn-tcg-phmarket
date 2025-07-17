e507078fb1c5dac5d3d519f292f0ad0e276bc462
Mark-AImagineers <148505322+Mark-AImagineers@users.noreply.github.com>
2025-07-17 15:13:55 +0800

    implemented docker

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,4 +1,16 @@
 from .base import *
+import os
 
 DEBUG = True
 ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "pokemon"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+    }
+}
