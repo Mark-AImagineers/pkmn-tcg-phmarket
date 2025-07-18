89b0b0a8e0500576d0d9f87258dccbde9f58160a
Mark-AImagineers <148505322+Mark-AImagineers@users.noreply.github.com>
2025-07-18 18:59:54 +0800

    added frontend login

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
-
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        "class": "w-full px-4 py-2 rounded bg-white/10 text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-green-400"
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        "class": "w-full px-4 py-2 rounded bg-white/10 text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-green-400"
+    }))
