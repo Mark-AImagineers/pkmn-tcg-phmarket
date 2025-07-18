7496b1f78799e3e11c3389c83a101b1bab89e125
Mark-AImagineers <148505322+Mark-AImagineers@users.noreply.github.com>
2025-07-18 16:55:54 +0800

    scaffold drf + HTMX + autodocs

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("users.urls")),
+    path("api/", include("users.api_urls")),  # for DRF API endpoints like /api/login/
+    path("", include("users.urls")),          # for HTMX views like /login/
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]
+
+
