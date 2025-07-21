7cf715d64d7f55b110f3a6787f935f9e5040add1
Mark-AImagineers <148505322+Mark-AImagineers@users.noreply.github.com>
2025-07-21 10:00:33 +0800

    style: redesign login page

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,22 @@
 from django import forms
 
+
 class LoginForm(forms.Form):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={
-        "class": "w-full px-4 py-2 rounded bg-white/10 text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-green-400",
-        "autocomplete": "email",
-        "id": "id_email"
-    }))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={
-        "class": "w-full px-4 py-2 rounded bg-white/10 text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-green-400",
-        "autocomplete": "current-password",
-        "id": "id_password"
-    }))
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "class": "w-full px-4 py-2 rounded bg-login-input text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-green-400",
+                "autocomplete": "email",
+                "id": "id_email",
+            }
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "w-full px-4 py-2 rounded bg-login-input text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-green-400",
+                "autocomplete": "current-password",
+                "id": "id_password",
+            }
+        )
+    )
